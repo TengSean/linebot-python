848a1d04902a06ae1483e5322bdc5cd2d8953175
TengSean <e125040121@gmail.com>
2020-06-19 16:49:50 +0800

    message api testing 22 fix show dir method

--- app.py
+++ app.py
@@ -153,10 +153,74 @@ def filter_message(message):
                 )
     elif message == 'ConfirmTemplate':
 #         pass
-        return 'ConfirmTemplate function'
+#         return 'ConfirmTemplate function'
+        return TemplateSendMessage(
+                    alt_text='Confirm template',
+                    template=ConfirmTemplate(
+                        text='Are you sure?',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='postback',
+                                text='postback text',
+                                data='action=buy&itemid=1'
+                            ),
+                            MessageTemplateAction(
+                                label='message',
+                                text='message text'
+                            )
+                        ]
+                    )
+                )
     elif message == 'CarouselTemplate':
 #         pass
-        return 'CarouselTemplate function'
+#         return 'CarouselTemplate function'
+        return TemplateSendMessage(
+                    alt_text='Carousel template',
+                    template=CarouselTemplate(
+                        columns=[
+                            CarouselColumn(
+                                thumbnail_image_url='https://example.com/item1.jpg',
+                                title='this is menu1',
+                                text='description1',
+                                actions=[
+                                    PostbackTemplateAction(
+                                        label='postback1',
+                                        text='postback text1',
+                                        data='action=buy&itemid=1'
+                                    ),
+                                    MessageTemplateAction(
+                                        label='message1',
+                                        text='message text1'
+                                    ),
+                                    URITemplateAction(
+                                        label='uri1',
+                                        uri='http://example.com/1'
+                                    )
+                                ]
+                            ),
+                            CarouselColumn(
+                                thumbnail_image_url='https://example.com/item2.jpg',
+                                title='this is menu2',
+                                text='description2',
+                                actions=[
+                                    PostbackTemplateAction(
+                                        label='postback2',
+                                        text='postback text2',
+                                        data='action=buy&itemid=2'
+                                    ),
+                                    MessageTemplateAction(
+                                        label='message2',
+                                        text='message text2'
+                                    ),
+                                    URITemplateAction(
+                                        label='uri2',
+                                        uri='http://example.com/2'
+                                    )
+                                ]
+                            )
+                        ]
+                    )
+                )
     elif message == 'ImageCarouselTemplate':
 #         pass
         return 'ImageCarouselTemplate function'
@@ -175,7 +239,7 @@ def filter_message(message):
                 'ImageCarouselTemplate'
         ''')
     elif message == 'show':
-        return TextSendMessage(text=os.listdir('./data'))
+        return TextSendMessage(text=', '.join(os.listdir('./data')))
     else:
         return TextSendMessage(text=message)
 
